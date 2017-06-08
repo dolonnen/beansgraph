b0cfd56ebe355acb40d8284759376567b4a207a9
Markus Barthel <markus.barthel.mb@gmail.com>
2017-06-08 23:54:47 +0200

    playing with functions to call the API from YT and Twitch

--- prototyping/js/prototype.js
+++ prototyping/js/prototype.js
@@ -1,2 +1,30 @@
- 
-alert("test");
+
+function call4ViewerCount() {
+    streamIdYoutube = "tn0zpeHPfw0"
+    apiKeyYoutube = "AIzaSyAEKdRMGaHEVQCTzLrUbX3HPdp82mTpWWk";
+    streamIdTwitch = "rocketbeanstv"
+    apiKeyTwitch = "vsiaqcev0wed3la13a05h3tyi93z2o";
+
+//     alert('1');
+    $.get(
+    "https://api.twitch.tv/kraken/streams/"+streamIdTwitch+"?client_id="+apiKeyTwitch,
+    processTwitchData
+    );
+    
+//     alert('2');
+    $.getJSON(
+    "https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id="+streamIdYoutube+"&key="+apiKeyYoutube,
+    processYtData
+    );
+}
+
+function processYtData(result, status) {
+    $("#youtube").html(result.items[0].liveStreamingDetails.concurrentViewers);
+}
+
+function processTwitchData(result, status) {
+    $("#twitch").html(result.stream.viewers);
+}
+
+
+call4ViewerCount();
