d1b5f00a534e69124c733ac8c6adbf119ad4b0cc
Markus Barthel <markus.barthel.mb@gmail.com>
2017-06-27 00:56:45 +0200

    beginning server side code

--- site/bin/getStreamCreds.py
+++ site/bin/getStreamCreds.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3.6
 
 from flask import Flask
+import requests
+import json 
+
 app = Flask(__name__)
+
+# API-Keys
+apiKeys = {
+'youtube': "AIzaSyAEKdRMGaHEVQCTzLrUbX3HPdp82mTpWWk",
+'twitch': "vsiaqcev0wed3la13a05h3tyi93z2o"
+}
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    
+    rbSite = requests.get('https://www.rocketbeans.tv/').text
+    
+    streamCreds['apikeys'] = apiKeys
+    return json.dumps(streamCreds)
 
+
+
+# https://www.youtube.com/embed/8bYu-0ErsfE?modestbranding=1&showinfo=0
+# https://player.twitch.tv/?channel=rocketbeanstv&autoplay=false
